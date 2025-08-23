7850d87418cb5b8036299444a913fe69521f9c4b
Aderinmola <favorsurpisesO1@gmail.com>
2025-08-23 22:28:40 +0100

    celery email config

--- eventnest/urls.py
+++ eventnest/urls.py
@@ -7,6 +7,8 @@ from .swagger import schema_view
 
 
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
