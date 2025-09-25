0bacdbe075fa613cceac32b069a4d29c224f3811
johnpauljpc <jpcwork081@gmail.com>
2025-09-25 17:24:37 +0100

    urls for tasks modified.

--- eventnest/urls.py
+++ eventnest/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     
     path("api/events/", include("apps.events.urls")), #urls for events
     path("api/contacts/", include("apps.contacts.urls")), #urls for Contacts
-    path("api/tasks/", include("apps.tasks.urls")), #urls for Tasks
+    path("api/events/", include("apps.tasks.urls")), #urls for Tasks
 ]
 
 
